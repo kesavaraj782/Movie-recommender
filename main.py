657e8cd5f2440448e9ef67b8bb8bc95015e0c6d2
Arjun P <72484657+arjunprakash027@users.noreply.github.com>
2022-09-22 10:51:02 +0530

    minor fix

--- main.py
+++ main.py
@@ -22,30 +22,22 @@ def recommend():
     #print(feature_vectors)
     similarity = cosine_similarity(feature_vectors)
     #print(similarity)
-    movie_name = st.text_input("Enter your movie:","batman")
-   # if movie_name not in movies_data['title'].tolist():
-    #    st.text("enter a movie name to start")
-    #else:
-    list_of_all_titles = movies_data['title'].tolist()
-    find_close_match = difflib.get_close_matches(movie_name,list_of_all_titles)
-    close_match = find_close_match[0]
-    index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
-    similarity_score = list(enumerate(similarity[index_of_the_movie]))
-    sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse=True)
-    list_of_all_titles = movies_data['title'].tolist()
-        #print(list_of_all_titles)
-    
-        #print(find_close_match)
-    
-    
-        #print(index_of_the_movie)
-        #print(similarity_score)
-        #print(sorted_similar_movies)
-    st.subheader("Movies suggested for you: \n")
-    i = 1
-    for movie in sorted_similar_movies:
-        index = movie[0]
-        title_from_index = movies_data[movies_data.index==index]['title'].values[0]
-        if(i<30):
-            st.text("{}".format (title_from_index))
-            i+=1   
+    try:
+        movie_name = st.text_input("Enter your movie:")
+        list_of_all_titles = movies_data['title'].tolist()
+        find_close_match = difflib.get_close_matches(movie_name,list_of_all_titles)
+        close_match = find_close_match[0]
+        index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
+        similarity_score = list(enumerate(similarity[index_of_the_movie]))
+        sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse=True)
+        list_of_all_titles = movies_data['title'].tolist()
+        st.subheader("Movies suggested for you: \n")
+        i = 1
+        for movie in sorted_similar_movies:
+            index = movie[0]
+            title_from_index = movies_data[movies_data.index==index]['title'].values[0]
+            if(i<30):
+                st.text("{}".format (title_from_index))
+                i+=1
+    except:
+        st.text("enter a movie name that is valid")   
