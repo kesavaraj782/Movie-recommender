5833c1a35d9dc89f0ec1719284b87d4b3f4922ad
kesavaraj782 <r.kesava782@gmail.com>
2022-09-22 10:46:03 +0530

    puck

--- main.py
+++ main.py
@@ -22,23 +22,30 @@ def recommend():
     #print(feature_vectors)
     similarity = cosine_similarity(feature_vectors)
     #print(similarity)
-    movie_name = st.text_input("Enter your movie:")
+    movie_name = st.text_input("Enter your movie:","batman")
+   # if movie_name not in movies_data['title'].tolist():
+    #    st.text("enter a movie name to start")
+    #else:
     list_of_all_titles = movies_data['title'].tolist()
-    #print(list_of_all_titles)
     find_close_match = difflib.get_close_matches(movie_name,list_of_all_titles)
-    #print(find_close_match)
     close_match = find_close_match[0]
     index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
-    #print(index_of_the_movie)
     similarity_score = list(enumerate(similarity[index_of_the_movie]))
-    #print(similarity_score)
     sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse=True)
-    #print(sorted_similar_movies)
+    list_of_all_titles = movies_data['title'].tolist()
+        #print(list_of_all_titles)
+    
+        #print(find_close_match)
+    
+    
+        #print(index_of_the_movie)
+        #print(similarity_score)
+        #print(sorted_similar_movies)
     st.subheader("Movies suggested for you: \n")
     i = 1
     for movie in sorted_similar_movies:
         index = movie[0]
         title_from_index = movies_data[movies_data.index==index]['title'].values[0]
         if(i<30):
-            st.text("{}".format(title_from_index))
+            st.text("{}".format (title_from_index))
             i+=1   
